98a7547c2fcdb51934347878abb7a09af579b05a
MuhammadHasham23 <muhammad.hasham.2311@gmail.com>
2023-10-25 10:14:37 +0200

    feat: added cart

--- src/Components/RiyadhForm.js
+++ src/Components/RiyadhForm.js
@@ -1,13 +1,11 @@
 import React from "react";
 import emptyCartImage from "../assets/images/emptyCart.svg";
-import moduladdToNothing from "../assets/images/bannerImages/addToNothing.svg";
 import grocery from "../assets/images/grocery.svg";
 import moveToTrash from "../assets/images/delete_icon.svg";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 import "../style.css";
-import { Input, Timepicker, initTE } from "tw-elements";
 
 const RIYADH_INIT_URL =
   "https://us-central1-arsann-f26bb.cloudfunctions.net/getViaRiyadhServices";
@@ -17,6 +15,14 @@ function RiyadhForm({ color }) {
   const [cartItem, setCartItem] = useState([]);
   const [services, setServices] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errors,setErrors] = useState({
+    name: false,
+    arrivalTime: false,
+    mobileNumber: false,
+    gate: false
+  })
+
+  const [isMobileCartVisible,setIsMobileCartVisible] = useState(false)
 
   // tailwind dropUp
 
@@ -37,9 +43,6 @@ function RiyadhForm({ color }) {
     gate: "",
   });
 
-  useEffect(() => {
-    console.log("form", form);
-  }, [form]);
   const addToCart = (item) => {
     setCartItem([...cartItem, item]);
 
@@ -72,6 +75,16 @@ function RiyadhForm({ color }) {
   };
 
   const bookPackage = async () => {
+    const validationErrors = {}
+    if(form.gate.length === 0) validationErrors['gate'] = true
+    if(form.name.length === 0) validationErrors['name'] = true
+    if(form.mobileNumber.length === 0) validationErrors['mobileNumber'] = true
+    if(form.arrivalTime.length === 0) validationErrors['arrivalTime'] = true
+    setErrors(validationErrors)
+    if(Object.keys(validationErrors).length > 0) {
+      return alert('Unable to book, please fill proper fields')
+    }
+    
     setIsLoading(true);
     const body = {
       amount: totalPrice,
@@ -81,11 +94,11 @@ function RiyadhForm({ color }) {
         phone: form.mobileNumber,
         services: cartItem,
       },
-      time: new Date().getTime(),
+      time: new Date(form.arrivalTime).getTime(),
     };
-    const result = await axios.post(RIYADH_BOOKING_URL, body);
-    if (result.data.redirectUrl) {
-      window.location.replace(result.data.redirectUrl);
+    const {data} = await axios.post(RIYADH_BOOKING_URL, body);
+    if (data.data.redirectUrl) {
+      window.location.replace(data.data.redirectUrl);
     }
     setIsLoading(false);
   };
@@ -124,10 +137,14 @@ function RiyadhForm({ color }) {
                         className="border-b border-gray-500 w-full px-4 py-2 bg-[#272727] text-white border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300"
                         required
                         value={form.name}
-                        onChange={(e) =>
+                        onChange={(e) => {
                           setForm({ ...form, name: e.target.value })
-                        }
+                          if(e.target.value?.length > 0) {
+                            setErrors({...errors, name: false})
+                          } 
+                        }}
                       />
+                      <span className="error-span">{errors.name && 'Please fill the required field'}</span>
                     </div>
                     <div className="mb-4">
                       {/* time selection menu */}
@@ -145,11 +162,16 @@ function RiyadhForm({ color }) {
                           className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="form2"
                           value={form.arrivalTime}
-                          onChange={(e) =>
+                          onChange={(e) => {
                             setForm({ ...form, arrivalTime: e.target.value })
-                          }
+                            if(e.target.value?.length > 0) {
+                              setErrors({...errors, arrivalTime: false})
+                            } 
+                          }}
                         />
+
                       </div>
+                      <span className="error-span">{errors.arrivalTime && 'Please fill the required field'}</span>
                     </div>
                   </div>
 
@@ -173,15 +195,21 @@ function RiyadhForm({ color }) {
                           className="border-b border-gray-500 w-full px-4 py-2 bg-[#272727] text-white border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300"
                           required
                           value={form.mobileNumber}
-                          onChange={(e) =>
+                          onChange={(e) => {
                             setForm({ ...form, mobileNumber: e.target.value })
-                          }
+                            if(e.target.value?.length > 0) {
+                              setErrors({...errors, mobileNumber: false})
+                            } 
+                          }}
                         />
+                       
                       </div>
+                      
 
                       <small className="text-gray-300">
                         Please enter a 10-digit phone number.
                       </small>
+                      <span className="error-span">{errors.mobileNumber && 'Please fill the required field'}</span>
                     </div>
                     {/* <!-- Option Menu --> */}
                     <div className="mb-5">
@@ -193,9 +221,12 @@ function RiyadhForm({ color }) {
                         name="option"
                         className="border-b  border-gray-500 w-full px-4 py-2  bg-[#272727] text-white border rounded-md focus:outline-none focus:ring focus:border-blue-300 border-gray-400"
                         required
-                        onChange={(e) =>
+                        onChange={(e) => {
                           setForm({ ...form, gate: e.target.value })
-                        }
+                          if(e.target.value?.length > 0) {
+                            setErrors({...errors, gate: false})
+                          } 
+                        }}
                         value={form.gate}
                       >
                         <option option="V1 – Main Entrance">
@@ -214,6 +245,7 @@ function RiyadhForm({ color }) {
                           V5 – Via Mercato
                         </option>
                       </select>
+                      
                     </div>
                   </div>
                 </form>
@@ -324,71 +356,74 @@ function RiyadhForm({ color }) {
       </div>
       {/* The DropUp Menu */}
       <div class="mob collapse-cart">
-        <button type="button" class="btn btn-primary up-arrow">
-          {/* <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo"> */}
+        <button type="button" class="btn btn-primary up-arrow" onClick={()=> setIsMobileCartVisible(!isMobileCartVisible)}>
+         
           <span>
             <img src={grocery} alt="" />
-            {/* <span
-              class="span-carItem position-absolute top-0 start-100 translate-middle badge rounded-pill"
-             
-            >
-             
-            </span> */}
           </span>
+          <span className="rounded-pill-items-count"><span>{cartItem.length}</span></span>
         </button>
-        <div
-          id="demo"
-          class="collapse collapse-div"
 
-          // id="divID"
-          // style="display: none"
-        >
-          <div class="col-sm-12 cart card right-sec">
-            <div class="">
-              <hr />
-              <div class="card-body p-0 hi-100">
-                <span>
-                  <div className="addNothing-div">
-                    <img src={moduladdToNothing} alt="" />
-                  </div>
-                  <p className="nothingInCart">Nothing in your cart</p>
-                </span>
-                <ng-template>
-                  <ul class="addCart ">
-                    <li>
-                      <span>
-                        <p className="serviceName">
-                          {/* {{ cart.service_name }} */}
-                        </p>
-                        <p className="priceDiv">
-                          {/* Price: {{ cart.service_price }} */}
-                        </p>
+        {isMobileCartVisible && <div className="cart-container-mobile cart bg-[#272727]  text-white  rounded-[35px] shadow-md w-25">
+                <h1 className="cart-heading text-3xl font-semibold  text-white mb-4  pt-5  pb-4 w-96 ml-6 ">
+                  Cart
+                </h1>
+
+                <div className="cart-item-container ">
+                  {cartItem.length > 0 ? (
+                    cartItem.map((item, index) => (
+                      <>
+                        <div
+                          key={index}
+                          className="cart-item flex items-center justify-between pl-6 pb-6"
+                        >
+                          <div className="flex flex-col">
+                            <h2 className="font-semibold text-18">
+                              {item.service_name}
+                            </h2>
+                            <p className="text-gray-500">
+                              Price: {item.service_price} SAR
+                            </p>
+                          </div>
+                          <div className="deleteButton pr-8">
+                            <button onClick={() => removeFromCart(index)}>
+                              <img src={moveToTrash} alt="" />
+                            </button>
+                          </div>
+                        </div>
+                      </>
+                    ))
+                  ) : (
+                    <>
+                      <div className="empty-cart-img flex items-center justify-center pt-32">
+                        <img src={emptyCartImage} alt="" />
+                      </div>
+                      <span className="flex  justify-center">
+                        Nothing in your cart
                       </span>
-                      <span>
-                        <img src="../../assets/image/trash.png" alt="" />
-                      </span>
-                    </li>
-                  </ul>
-                </ng-template>
-              </div>
-              <ng-template>
-                <div class="card-footer p-0 cardFooter">
-                  <ul>
-                    <li>
-                      <span>
-                        <p className="liSpan">Total Amount</p>
-                      </span>
-                      <span>{/* <p>{{ this.total }} SAR</p> */}</span>
-                    </li>
-                  </ul>
-                  <button type="button" class="btn btn-gold bookButton">
-                    Book
-                  </button>
+                    </>
+                  )}
                 </div>
-              </ng-template>
-            </div>
-          </div>
-        </div>
+                {cartItem.length > 0 ? (
+                  <div className="total-container  pl-6 h-[5rem] ">
+                    <div className="total-div flex justify-between  border-t border-gray-500 w-[386px]">
+                      <h4 className="font-semibold ">Total</h4>
+                      <h4 className="font-semibold ">{totalPrice} SAR</h4>
+                    </div>
+
+                    <div className="book-button  py-[0.7rem] flex justify-center pr-9">
+                      <button
+                        className="addToCart bg-[#D4B066] text-white rounded-[1rem] w-56 text-[0.9rem] py-[0.7rem] hover:text-black transition-color duration-300"
+                        onClick={() => bookPackage()}
+                      >
+                        Book
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>}
       </div>
     </div>
   );
